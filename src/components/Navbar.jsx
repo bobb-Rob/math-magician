796435127331832ed4a0b57550c82edc906c3246
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-06 17:39:43 +0100

    add menu button on mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HiMenu } from 'react-icons/hi';
 
 const Navbar = () => (
-  <nav>
+  <nav className="navbar">
     <h1>Math Magicians</h1>
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/calculator">Calculator</Link>
-      <Link to="/quotes">Quotes</Link>
+    <div className="desktop-nav">
+      <Link to="/" className="link">Home</Link>
+      <Link to="/calculator" className="link">Calculator</Link>
+      <Link to="/quotes" className="link">Quotes</Link>
     </div>
+    <button type="button" className="mobile-menu">
+      <HiMenu color="#4d4c4c" size="1.8rem" />
+    </button>
   </nav>
 );
 
